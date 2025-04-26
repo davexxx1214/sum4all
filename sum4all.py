2ee5b4b545eb85d0506d8368463ebe1463cccda1
davexxx <davexxx@163.com>
2025-04-26 08:19:44 +0800

    修正识图数量问题。

--- sum4all.py
+++ sum4all.py
@@ -366,6 +366,7 @@ class sum4all(Plugin):
                         self.params_cache[user_id]['image_sum_quota'] -= 1
                 else:
                     if self.params_cache[user_id]['image_sum_quota'] > 0:
+                        self.params_cache[user_id]['image_sum_quota'] = 0
                         self.handle_openai_image(base64_image, e_context)
 
                 if self.params_cache[user_id]['image_sum_en_quota'] > 0:
